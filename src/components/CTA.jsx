4715f83eaa2a522e7059445ea9a43280a30b42e7
Ayomide Yerokun <Ayomideyerokun54@gmail.com>
2026-05-22 20:01:12 +0100

    Updated

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 const CTA = () => {
   return (
     <>
-      <div className=" md:h-80 p-4 md:p-9 mb-10">
-        <div className="h-full w-full rounded-tl-4xl rounded-br-3xl bg-orange-400 p-10 flex flex-col justify-center items-center">
+      <div className="h-full p-4 md:p-9 mb-10">
+        <div className="w-full rounded-tl-4xl rounded-br-3xl bg-orange-400 p-10 flex flex-col justify-center items-center">
           <h2 className="px-2 w-2xs md:w-lg font-brand leading-tight mb-3 font-extrabold text-4xl text-center">
             Let's build something great together
           </h2>
