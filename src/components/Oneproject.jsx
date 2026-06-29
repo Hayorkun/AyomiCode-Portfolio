6d6709b57fd3f8871493b0c084b2a75d5248ed51
Ayomide Yerokun <Ayomideyerokun54@gmail.com>
2026-06-29 11:06:33 +0100

    FIX: Live link button added to the project card

--- src/components/Oneproject.jsx
+++ src/components/Oneproject.jsx
@@ -1,3 +1,5 @@
+import { Link } from "lucide-react";
+
 function Oneproject({ project }) {
   return (
     <div className="h-full rounded-2xl border border-gray-50 hover:shadow-orange-200 hover:shadow-lg transition-transform duration-300 hover:scale-102 ease-in-out">
@@ -9,9 +11,13 @@ function Oneproject({ project }) {
         />
       </div>
       <div className="p-3 w-fit">
-        <p className="w-fit py-2 px-3 border border-orange-400/80 rounded-3xl font-body text-xs text-orange-400 font-extrabold mb-1">
+       <div className="flex items-center justify-between">
+         <p className="w-fit py-1 px-2 border border-orange-400/80 rounded-3xl font-body text-xs text-orange-400 font-semibold mb-1">
           Web App
         </p>
+        <a href={project.livelink}
+         className="w-fit py-1 px-2 border border-white/30 rounded-md font-body text-xs text-orange-400 mb-1 flex gap-1 items-center"><Link className="size-3 stroke-white/50"/>Live link</a>
+       </div>
         <h3 className="font-body font-bold text-xl leading-relaxed mb-1 text-white/80">{project.name}</h3>
         <p className="font-body text-sm text-gray-400 ">{project.description}</p>
       </div>
